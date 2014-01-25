3c32c6d3431ad75bca0cd047804a2c99e88b3bed
Wit <wit.szoniec@uj.edu.pl>
2014-01-25 17:49:09 +0100

    Wszystko w pliku utils.h

--- utils.h
+++ utils.h
@@ -1,8 +1,19 @@
 #ifndef UTILS_H
 #define UTILS_H
-#include <list>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <list>
 using namespace std;
+
+list<int> Generator()
+{
+    list<int> L;
+    srand(time(NULL));
+    for(int i=0; i<10; ++i)
+        L.push_back(rand()%100+1);
+    return L;
+}
 
 void Print(list<int> L)
 {
@@ -15,9 +26,31 @@ void Print(list<int> L)
 int Sum(list<int> L)
 {
     int s = 0;
-    for(list<int>::iterator it=L.begin(); it!=L.end(); ++i)
+    for(list<int>::iterator it=L.begin(); it!=L.end(); ++it)
         s += (*it);
     return s;
+}
+
+int Min(list<int> L)
+{
+	int tmp = L.front();
+	for (list<int>::iterator it = L.begin() ; it !=L.end(); ++it)
+        {
+		if (tmp > *it)
+			tmp = *it;
+	}
+	return tmp;
+}
+
+int Max(list<int> L)
+{
+	int tmp = L.front();
+	for (list<int>::iterator it = L.begin() ; it !=L.end(); ++it)
+        {
+		if (tmp < *it)
+			tmp = *it;
+	}
+	return tmp;
 }
 
 #endif
